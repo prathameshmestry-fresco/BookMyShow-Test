e184e378fcc8228a6278027e52110545a43882be
Prathamesh Mestry <prathameshmestry1992@gmail.com>
2021-06-04 10:49:08 +0530

    Initial Commit

---
+++ BookMyShow-Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookMyShow-Test
+//
+//  Created by Prathamesh Mestry on 04/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

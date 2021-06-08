22a6ffd17915afacff612df0e33f9e2141de7603
Prathamesh Mestry <prathameshmestry1992@gmail.com>
2021-06-08 18:37:17 +0530

    Added Similar Movies collection view cell.

---
+++ BookMyShow-Test/Movie Detail/Views/SimilarMoviesCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SimilarMoviesCollectionViewCell.swift
+//  BookMyShow-Test
+//
+//  Created by Prathamesh Mestry on 08/06/21.
+//
+
+import UIKit
+
+class SimilarMoviesCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}

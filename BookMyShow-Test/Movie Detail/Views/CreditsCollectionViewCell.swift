7b4a109f46a6c17ba4485d46dff83ea3af7b18c9
Prathamesh Mestry <prathameshmestry1992@gmail.com>
2021-06-08 18:21:53 +0530

    Added collection view cell for Credits.

---
+++ BookMyShow-Test/Movie Detail/Views/CreditsCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CreditsCollectionViewCell.swift
+//  BookMyShow-Test
+//
+//  Created by Prathamesh Mestry on 07/06/21.
+//
+
+import UIKit
+
+class CreditsCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
